a3f724925fca86607a3981f02f0b3d5a8c5958e0
HotLava03 <tierriff@gmail.com>
2021-08-05 13:18:14 +0100

    Add dependencies. Create detail activity.

--- app/src/main/java/io/github/ceruleanapp/androidapp/DetailsActivity.java
+++ app/src/main/java/io/github/ceruleanapp/androidapp/DetailsActivity.java
@@ -3,11 +3,11 @@ package io.github.ceruleanapp.androidapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class DetailsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
     }
 }
